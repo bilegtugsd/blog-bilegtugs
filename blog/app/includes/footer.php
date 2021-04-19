6a43b02c3aa968c27dc57154d60367244c72727a
bilegtugsd <82795515+bilegtugsd@users.noreply.github.com>
2021-04-19 15:04:38 +0800

    Add files via upload

---
+++ blog/app/includes/footer.php
@@ -0,0 +1,64 @@
+ <!-- footer -->
+ <div class="footer">
+    <div class="footer-content">
+
+      <div class="footer-section about">
+        <h1 class="logo-text"><span>Bilegtugs's</span>blog</h1>
+        <p>
+          Ene blog site deeree bi oort taaalagdsan bolon ta nart heregtei gej bodson medeelluude oruulj bh bolno.
+          
+        </p>
+        <div class="contact">
+          <span><i class="fas fa-phone"></i> &nbsp; +976 89396399</span>
+          <span><i class="fas fa-envelope"></i> &nbsp; c20io1007@ufe.edu.mn</span>
+        </div>
+        <div class="socials">
+          <a href="#"><i class="fab fa-facebook"></i></a>
+          <a href="#"><i class="fab fa-instagram"></i></a>
+          <a href="#"><i class="fab fa-twitter"></i></a>
+          <a href="#"><i class="fab fa-youtube"></i></a>
+        </div>
+      </div>
+
+      <div class="footer-section links">
+        <h2>Quick Links</h2>
+        <br>
+        <ul>
+          <a href="#">
+            <li>Events</li>
+          </a>
+          <a href="#">
+            <li>Team</li>
+          </a>
+          <a href="#">
+            <li>Mentores</li>
+          </a>
+          <a href="#">
+            <li>Gallery</li>
+          </a>
+          <a href="#">
+            <li>Terms and Conditions</li>
+          </a>
+        </ul>
+      </div>
+
+      <div class="footer-section contact-form">
+        <h2>Contact us</h2>
+        <br>
+        <form action="index.html" method="post">
+          <input type="email" name="email" class="text-input contact-input" placeholder="Your email address...">
+          <textarea rows="4" name="message" class="text-input contact-input" placeholder="Your message..."></textarea>
+          <button type="submit" class="btn btn-big contact-btn">
+            <i class="fas fa-envelope"></i>
+            Send
+          </button>
+        </form>
+      </div>
+
+    </div>
+
+    <div class="footer-bottom">
+      &copy; IO institute Mongolia | Designed by Bilegtugs
+    </div>
+  </div>
+  <!-- // footer -->
